048a766d5d8e12ce8ef36befb1f09fca80527991
Javier Esaú Castillo Villalobos <21000643@alumnos.utleon.edu.mx>
2024-09-14 10:29:10 -0600

    Commit final

--- src/temas-ts/identificarTriangulo.ts
+++ src/temas-ts/identificarTriangulo.ts
@@ -1,33 +1,25 @@
 import {d2p} from './distanciaEntre2Puntos';
 
 class Triangulo extends d2p{
-    ladoA:number=0;
-    ladoB:number=0;
-    ladoC:number=0;
-    constructor(d1x:number, d2x:number, d1y:number, d2y:number){
-        super(d1x,d2x,d1y,d2y)
+    d3x:number;
+    d3y:number;
+    constructor(d1x:number, d1y:number, d2x:number, d2y:number, d3x:number, d3y:number){
+        super(d1x,d1y,d2x,d2y)
+        this.d3x=d3x
+        this.d3y=d3y
     }
     obtenerLado(){
-        const lA = new d2p(-4,-3,2,5);
-        this.ladoA = lA.distancia()
-        const lB = new d2p(-4,-3,2,6);
-        this.ladoB = lB.distancia()
-        const lC = new d2p(-4,-3,2,7);
-        this.ladoC = lC.distancia()
-        console.log(`Lado A: ${this.ladoA}; Lado B: ${this.ladoB}; Lado C: ${this.ladoC}`)
-        if((this.ladoA+this.ladoB)>this.ladoC || (this.ladoB+this.ladoC)>this.ladoA || (this.ladoC+this.ladoA)>this.ladoB)
-            console.log(`Es triángulo`)
-        else
-            console.log(`No es triángulo`)
+        const ladoA = new d2p(this.d1x, this.d1y, this.d2x, this.d2y).distancia();
+        const ladoB = new d2p(this.d2x, this.d2y, this.d3x, this.d3y).distancia();
+        const ladoC = new d2p(this.d3x, this.d3y, this.d1x, this.d1y).distancia();
+        console.log(`Lado A: ${ladoA}; Lado B: ${ladoB}; Lado C: ${ladoC}`)
+        if((ladoA+ladoB)>ladoC && (ladoB+ladoC)>ladoA && (ladoC+ladoA)>ladoB){
+            console.log(`los puntos: (${this.d1x}, ${this.d1y}), (${this.d2x}, ${this.d2y}), (${this.d3x}, ${this.d3y}): forman un triángulo`);
+        }else{
+            console.log(`No es posible construirlo`)
+        }
     }
 }
 
-const isTriangulo = new Triangulo(-4,-3,2,5);
+const isTriangulo = new Triangulo(0,0,4,0,4,3);
 isTriangulo.obtenerLado();
-
-/*const distancia1 = new d2p(-4,-3,2,5);
-console.log(`Punto 1: ${distancia1.distancia()}`)
-const distancia2 = new d2p(-4,-3,2,5);
-console.log(`Punto 2: ${distancia2.distancia()}`)
-const distancia3 = new d2p(-4,-3,2,5);
-console.log(`Punto 3: ${distancia3.distancia()}`)*/
